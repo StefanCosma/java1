216491d280471fa021a281a99d74c74030f18d46
StefanCosma <stefcos@yahoo.com>
2019-05-23 20:43:12 +0300

    d

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        char myChar = 1;
+        byte mybyte = 100;
+        short myshort = 10000;
+
+        System.out.println(4 + 4);
+        System.out.println(5 - 2);
+        System.out.println(10 + 10 -10);
+
+
+
+
+
+
+
+
+
+    }
+}
