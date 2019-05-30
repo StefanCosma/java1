1dc6dffadbb14c2a6fd8ec18b5552b6534bd5bd9
StefanCosma <stefcos@yahoo.com>
2019-05-30 18:27:24 +0300

    d

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
         char myChar = 1;
         byte mybyte = 100;
@@ -11,10 +11,22 @@ public class Main {
 
         System.out.println(4 + 4);
         System.out.println(5 - 2);
-        System.out.println(10 + 10 -10);
+        System.out.println(10 + 10 - 10);
+        System.out.println(-5 + (8 * 6));
+        System.out.println(-5 + (8 * 6));
+        System.out.println((55 + 9) % 9);
 
 
+        // tema
 
+        System.out.println("Hallo \nStefan");
+        System.out.println(10 + 10 );
+        System.out.println(50 / 5 );
+
+        System.out.println(-5 + (8 * 6));
+        System.out.println((55 + 9) % 9);
+        System.out.println(20 + (- 3 * 5) / 8);
+        System.out.println(5 + 15 / (3 * 2) - (8 % 3));
 
 
 
